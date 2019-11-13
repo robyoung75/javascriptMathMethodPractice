1c0773b3e1ec71b079b7256ef535546abacdb489
rob <52644668+robyoung75@users.noreply.github.com>
2019-11-13 12:00:22 -0700

    Math.ceil example

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ console.log(Math.round(num)); //logs 1 rounds to nearest number
 var num = 6.9;
 console.log(Math.floor(num)); //logs 6, rounds to lowest whole number
 
+//math round ceiling method
+var num = 6.1;
+console.log(Math.ceil(num)); // logs 7, rounds to highest whole number
+
+
 
 
 
