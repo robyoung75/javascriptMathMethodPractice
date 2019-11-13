45c326d6fedb6043e76a48027b4c23aa6645551b
rob <52644668+robyoung75@users.noreply.github.com>
2019-11-13 11:58:31 -0700

    Math.floor method example

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 //math round method
 var num = 1.3;
-console.log(Math.round(num)); //logs 1
+console.log(Math.round(num)); //logs 1 rounds to nearest number
+
+//math round floor method
+var num = 6.9;
+console.log(Math.floor(num)); //logs 6, rounds to lowest whole number
+
+
+
+
 
 
